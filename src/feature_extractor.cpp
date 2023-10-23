3b6594fe35f51e62f492d22d5d241a460de3beed
TaozheLi <Taozhe.Li-1@ou.edu>
2023-10-23 13:44:17 -0500

    updated

--- src/feature_extractor.cpp
+++ src/feature_extractor.cpp
@@ -264,6 +264,7 @@ std::vector<cv::Mat> FeatureExtractor::describeBRIEF(const cv::Mat &im, const st
     size_t k = 0;
     for( size_t i = 0 ; i < nbkps ; i++ ) 
     {
+        std::cout<<"in loop"<<std::endl;
         if( k < vkps.size() ) {
             if( vkps[k].pt == vpts[i] ) {
                 // vdescs.push_back(descs.row(k).clone());
